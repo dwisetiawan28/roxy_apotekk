c2344d8817f9093a1c0ac21ace2a4c580aec71ba
YugaAI <yugaadiirawan1@gmail.com>
2025-05-02 19:30:18 +0700

    fitur register

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,58 @@
 import java.sql.Connection;
 import config.DatabaseConfig;
 
+import model.user;
+import repository.usersRepo;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Connection conn = DatabaseConfig.connect();
-        if (conn != null) {
-            System.out.println("Koneksi ke database berhasil!");
-        } else {
-            System.out.println("Gagal terkoneksi ke database.");
+//        Connection conn = DatabaseConfig.connect();
+//        if (conn != null) {
+//            System.out.println("Koneksi ke database berhasil!");
+//        } else {
+//            System.out.println("Gagal terkoneksi ke database.");
+//        }
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("=== Registrasi User Baru ===");
+        System.out.print("Username       : ");
+        String username = sc.nextLine();
+
+        System.out.print("Email          : ");
+        String email = sc.nextLine();
+
+        System.out.print("Password       : ");
+        String password = sc.nextLine();
+
+        System.out.print("Phone Number   : ");
+        String phoneNumber = sc.nextLine();
+
+        System.out.print("Created By     : ");
+        String createdBy = sc.nextLine();
+        
+        LocalDateTime now = LocalDateTime.now();
+        
+        user user=new user(
+            username,
+            email,
+            password,
+            phoneNumber,
+            now,
+            now,
+            createdBy,
+            createdBy
+        );
+        
+        usersRepo repo=new usersRepo();
+        boolean success = repo.registerUser(user);
+        
+        if(success){
+        System.out.println("User berhasil didaftarkan.");
+        }else{
+        System.out.println("Gagal mendaftarkan user.");
         }
     }
 }
---
+++ src/main/java/repository/usersRepo.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repository;
+
+/**
+ *
+ * @author User
+ */
+
+import config.DatabaseConfig;
+import model.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class usersRepo {
+    public boolean registerUser(user users){
+        String sql ="INSERT INTO users(username, email, password, phone_number, created_at, created_by, updated_at, updated_by)"+"VALUES(?,?,?,?,?,?,?,?)";
+            try(Connection conn = DatabaseConfig.connect();
+                    PreparedStatement stmt=conn.prepareStatement(sql)){
+            
+                if(conn == null){
+                    System.err.println("Koneksi null. Gagal menyimpan user.");
+                    return false;
+                }
+                
+                stmt.setString(1, users.getUserName());
+                stmt.setString(2, users.getEmail());
+                stmt.setString(3, users.getPassword());
+                stmt.setString(4, users.getPhoneNumber());
+                stmt.setTimestamp(5, Timestamp.valueOf(users.getCreatedAt()));
+                stmt.setTimestamp(6, Timestamp.valueOf(users.getUpdatedAt()));
+                stmt.setString(7, users.getCreatedBy());
+                stmt.setString(8, users.getUpdatedBy());
+                
+                int rows = stmt.executeUpdate();
+                return rows>0;
+            }catch (SQLException e) {
+                System.err.println("❌ Error SQL: " + e.getMessage());
+                return false;
+            }
+    }
+}
