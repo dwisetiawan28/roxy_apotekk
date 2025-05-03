d253d2847b0d7b4a0e40a67fc423af8d9bcc8815
RhmnDev14 <rahmandev140302@gmail.com>
2025-05-04 00:56:29 +0700

    init ui login

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,11 @@
-import java.sql.Connection;
-import config.DatabaseConfig;
-
-import model.user;
-import repository.usersRepo;
 import ui.SignIn;
-import ui.SignUp;
 
-import java.util.Scanner;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1. Sign Up");
-        System.out.println("2. Sign In");
-        System.out.print("Pilih opsi: ");
-        int pilihan = Integer.parseInt(sc.nextLine());
-        
-        switch(pilihan){
-            case 1:
-                SignUp signUp = new SignUp();
-                signUp.show();
-                break;
-            case 2:
-                SignIn signIn = new SignIn();
-                signIn.show();
-                break;
-            default:
-                System.out.println("Pilihan tidak falid");
-        }
+        SwingUtilities.invokeLater(() -> {
+            new SignIn().setVisible(true);
+        });
     }
 }
