f8687fea7b400cf964f4c72dd2c259a1e976ff01
RhmnDev14 <rahmandev140302@gmail.com>
2025-05-04 09:52:02 +0700

    refactor ui login

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
-import ui.SignIn;
+import ui.Login;
 
-import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new SignIn().setVisible(true);
+        javax.swing.SwingUtilities.invokeLater(() ->{
+            //buat jframe
+            JFrame frame = new JFrame("INVENTORY APOTEK ROXY");
+            
+            //JPanel Login ke JFrame
+            frame.setContentPane(new Login());
+            
+            // atur JFrame
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
     }
 }
